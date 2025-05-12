bb94e7f9ed57a6447ddba1b6fdc34c13f4d77bdb
Spicyblue <ochulor.okechukwu@gmail.com>
2025-05-12 15:22:36 +0200

    Libft functions

---
+++ ft_isalnum.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: okochulo <okochulo@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 15:16:50 by okochulo          #+#    #+#             */
+/*   Updated: 2025/05/12 15:21:47 by okochulo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <unistd.h>
+# include <stdio.h>
+# include "libft.h"
+
+int ft_isalnum(int c )
+{
+    if (c == EOF)
+        return (0);
+    if ('a' <= c && c <= 'z')
+        return (1);
+    if ('A' <= c && c <= 'Z')
+        return (1);
+    if ('0' <= c && c <= '9')
+        return (1);
+    return (0);
+}
