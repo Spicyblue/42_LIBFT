c54cef5e60f2b654bb5eb164763982e5e3740adc
Spicyblue <ochulor.okechukwu@gmail.com>
2025-05-12 14:12:31 +0200

    Libft functions

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: okochulo <okochulo@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/29 16:37:16 by okochulo          #+#    #+#             */
-/*   Updated: 2025/05/12 14:11:52 by okochulo         ###   ########.fr       */
+/*   Created: 2025/05/12 14:04:03 by okochulo          #+#    #+#             */
+/*   Updated: 2025/05/12 14:11:34 by okochulo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 # include <unistd.h>
 # include <stdio.h>
 # include "libft.h"
 
-int ft_isalpha(int c )
+int ft_isdigit(int c )
 {
     if (c == EOF)
         return (0);
-    if ('a' <= c && c <= 'z')
-        return (1);
-    if ('A' <= c && c <= 'Z')
+    if ('0' <= c && c <= '9')
         return (1);
     return (0);
 }
