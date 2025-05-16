5f4735c6846354f1712e20d9159172c0b20cd182
ochuloro <ochulor.okechukwu@gmail.com>
2025-05-16 14:46:27 +0200

    Libft functions

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
@@ -6,21 +6,21 @@
 /*   By: okochulo <okochulo@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:37:16 by okochulo          #+#    #+#             */
-/*   Updated: 2025/05/12 14:11:52 by okochulo         ###   ########.fr       */
+/*   Updated: 2025/05/16 14:35:44 by okochulo         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
-# include <unistd.h>
-# include <stdio.h>
-# include "libft.h"
+#include <unistd.h>
+#include <stdio.h>
+#include "libft.h"
 
-int ft_isalpha(int c )
+int	ft_isalpha(int c)
 {
-    if (c == EOF)
-        return (0);
-    if ('a' <= c && c <= 'z')
-        return (1);
-    if ('A' <= c && c <= 'Z')
-        return (1);
-    return (0);
+	if (c == EOF)
+		return (0);
+	if ('a' <= c && c <= 'z')
+		return (1);
+	if ('A' <= c && c <= 'Z')
+		return (1);
+	return (0);
 }
