25fe2e54c6cebc92556633bdca26a6e857421dd3
ochuloro <ochulor.okechukwu@gmail.com>
2025-05-22 14:06:22 +0200

    Norm Verified Libft Functions

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -14,9 +14,37 @@
 
 int	ft_isdigit(int c)
 {
-	if (c == NULL)
+	if (c == EOF)
 		return (0);
 	if ('0' <= c && c <= '9')
 		return (1);
 	return (0);
 }
+/*
+#include <ctype.h>  
+
+int main()
+{
+    // Test cases
+    char tests[] = {'3', 'a', '9', 'Z', '0', '5'};
+    size_t num_tests = sizeof(tests) / sizeof(tests[0]);
+
+    // Using a while loop to test each character
+    size_t i = 0;
+    while (i < num_tests)
+    {
+        char test = tests[i];
+        printf("Test '%c': ft_isdigit: %d, isdigit: %d\n",
+             test, ft_isdigit(test), isdigit(test));
+        i++;
+    }
+
+    return (0);
+}
+*/
+/*
+The ft_isdigit function checks if the given character is a digit (0-9).
+It returns 1 if the character is a digit and 0 otherwise.
+The function also checks if the character is EOF (end of file)
+and returns 0 in that case.
+*/
